30f753486b6aa0811393e8e9af5634b1d3d413cd
Carter <cwest0997@gmail.com>
2026-01-18 05:19:38 -0500

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace noDamage
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(BoatDamage))]
+        private static class BoatDamagePatch
+        {
+            // Block collision damage
+            [HarmonyPatch("Impact")]
+            [HarmonyPrefix]
+            public static bool ImpactPrefix()
+            {
+                return false;
+            }
+
+            // Block daily wear damage
+            [HarmonyPatch("DailyDamage")]
+            [HarmonyPrefix]
+            public static bool DailyDamagePrefix()
+            {
+                return false;
+            }
+
+            // Block water overflow from waves
+            [HarmonyPatch("Overflow")]
+            [HarmonyPrefix]
+            public static bool OverflowPrefix()
+            {
+                return false;
+            }
+
+            // Reset damage values on Start
+            [HarmonyPatch("Start")]
+            [HarmonyPostfix]
+            public static void StartPostfix(BoatDamage __instance)
+            {
+                __instance.hullDamage = 0f;
+                __instance.waterLevel = 0f;
+                __instance.durabilityDays = 999999f;
+                __instance.impactDamageMult = 0f;
+                __instance.wearSteepness = 0f;
+                __instance.waterIntakeRate = 0f;
+                __instance.minimumImpactVelocity = 99999f;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace NoDamage
+{
+    [BepInPlugin(PLUGIN_ID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_ID = "com.headpilgrim.nodamage";
+        public const string PLUGIN_NAME = "No Ship Damage";
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        private void Awake()
+        {
+            Logger.LogInfo("No Ship Damage mod loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
+        }
+    }
+}
